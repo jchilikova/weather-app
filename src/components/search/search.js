80cfc715543a69ecd9dd3421e8fba8736d2ac947
jchilikova <jchilikova@gmail.com>
2022-11-16 14:36:16 +0200

    Added search, forecast and current-weather components

---
+++ src/components/search/search.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = () => {
+  const [search, setSearch] = useState(null);
+  
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+    />
+  );
+};
+
+export default Search;
