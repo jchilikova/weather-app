8cc4110cee196cd0bcdb444b4c02c63d1fbe82f9
jchilikova <jchilikova@gmail.com>
2022-11-16 15:53:24 +0200

    Added reponse to the loadOptions fetch

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,12 +7,21 @@ const Search = ({ onSearchChange }) => {
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=100&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
-      .then((response) => console.log(response))
-      };
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
