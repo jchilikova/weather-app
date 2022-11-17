c8e76e6025d40ff4a5b07c10b360a3f8ee60ac7f
jchilikova <jchilikova@gmail.com>
2022-11-17 15:06:10 +0200

    Added icons and some details for the weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -15,6 +15,32 @@ const CurrentWeather = ({ data }) => {
           src={`icons/${data.weather[0].icon}.png`}
         />
       </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
